3379ef49268eb9179c1f1b99c5c45f65454e4c82
Fr3Chr <166957195+Fzz1n@users.noreply.github.com>
2026-04-06 11:55:14 +0200

    string converters

--- src/type_check.py
+++ src/type_check.py
@@ -2,7 +2,7 @@ def check(AST):
     pass
 
 x = 8
-y = "2"
+y = 2
 z = x+y
 def is_numeric_value(v):
     if isinstance(v, bool):
@@ -17,24 +17,31 @@ def is_numeric_value(v):
             return False
     return False
 
-
-if is_numeric_value(x) and is_numeric_value(y):
-    print(z)
-else:
-    print("F, No")
-
-if isinstance(x, int) and isinstance(y, int):
-    print("yes, int")
-elif isinstance(x, str) and isinstance(y, str):
-    print("yes, string")
-elif isinstance(x, bool) and isinstance(y, bool):
-    print("yes, bool")
-elif isinstance(x, float) and isinstance(y, float):
-    print("yes, float")
-else:
-    print("Ff, No")
-
     #sortering af typecheckeren (bools, ints, floats that can be added to ints)
 
     #dynamic, so you should be able to change types during runtime (find out if its typechecker or just parser)
     #how to do the shit on the line aboce
+
+def to_int(s):
+    try:
+        num = int(s)
+        return num
+    except ValueError:
+        return ""
+def to_float(s):
+    try:
+        num = float(s)
+        return num
+    except ValueError:
+        return ""
+def to_bool(s):
+    try:
+        num = bool(s)
+        return num
+    except ValueError:
+        return ""
+str1 = to_int("1")
+str2 = to_float("1")
+str3 = to_bool("1")
+
+print(str1, str2, str3)

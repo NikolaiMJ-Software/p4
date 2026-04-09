79b21482c3051f86e1201111a5c211674754d066
Fr3Chr <166957195+Fzz1n@users.noreply.github.com>
2026-04-09 09:49:37 +0200

    type checker expansion

--- src/type_check.py
+++ src/type_check.py
@@ -1,28 +1,50 @@
-def check(AST):
-    pass
+from src.parser import Add, Mul, Define, Create, Div
 
-# Get the type of a value
-def get_type(v):
-    # Bool
-    if v in ["false", "true"]:
-        return bool
-    
-    # Int
-    try:
-        int(v)
-        return int
-    except:
-        pass
-    
-    # Float
-    try:
-        float(v)
-        return float
-    except:
-        pass
-    
-    # Else string
-    return str
+def check(node, indent=0):
+    print("  " * indent, node)
+    body = getattr(node, "body", None)
+    if body:
+        for child in body:
+            check(child, indent + 1)
+    elif getattr(node, "value", None):
+        val = getattr(node, "value", None)
+        valid_type = isinstance(val, int) or isinstance(val, float) or isinstance(val, str) or isinstance(val, bool)
+        if not valid_type:
+            check(val, indent +1 )
+        #print(f"-------------> {node.value}", valid_type)
+    else:
+        valid_type = isinstance(node, int) or isinstance(node, float) or isinstance(node, str) or isinstance(node, bool)
+        if valid_type:
+            return node
+        else:
+            #print("VALUES: ", node.left, node.right)
+            
+            handler = handlers.get(type(node))
+            if handler:
+                print(handler(node))
+            else:
+                check(node.left)
+                check(node.right)
+            
+            '''if node.left and not inspect.isclass(node.left):
+                check(node.left, indent + 1)
+            if node.right and not inspect.isclass(node.right):
+                check(node.right, indent + 1)'''
+                
+
+def check_add(node):
+    return check_op(node.left, "+", node.right)
+def check_min(node):
+    return check_op(node.left, "-", node.right)
+def check_mul(node):
+    return check_op(node.left, "*", node.right)
+def check_div(node):
+    return check_op(node.left, "/", node.right)
+handlers = {
+    Add: check_add,
+    Mul: check_mul,
+    Div: check_div,
+}
 
 # Check if 't' is a in or float
 def is_numeric(t):
@@ -30,8 +52,8 @@ def is_numeric(t):
 
 def check_op(val1, op, val2):
     # Get the type of the values
-    t1 = get_type(val1)
-    t2 = get_type(val2)
+    t1 = type(val1)
+    t2 = type(val2)
     
     # Connect only string with a string
     invalid_str = (t1 == str) != (t2 == str)

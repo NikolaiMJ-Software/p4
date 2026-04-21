e31a8925fd32b70209b133c14a56fac8f48d11ba
Nikolai Medom Jensen <nikolai456654@gmail.com>
2026-04-21 14:25:55 +0200

    test virker wuhuu

--- src/parser.py
+++ src/parser.py
@@ -26,7 +26,7 @@ start: stmt*
 // STATEMENTS
 create_stmt: "create" ID var_tail NEWLINE -> create_v
     | "create" ID struct_tail -> create_s
-    | "create" ID list_tail -> create_l
+    | "create" ID "is" list_tail -> create_l
 
 var_tail: ("is" expr)?
 
@@ -39,7 +39,7 @@ struct_field: ID NEWLINE
     | ID "is" expr NEWLINE
     | ID "is" "listing:" list_items? NEWLINE
 
-list_tail: "is listing:" list_items? NEWLINE
+list_tail: "listing:" list_items? NEWLINE
 list_items: list_item ("," list_item)*
 
 assign_stmt: ID inheritance "is" expr NEWLINE -> assign_v

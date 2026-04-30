801a48c16c06d933632adba109fddae988983087
Alenje2004 <WS10ZL@student.aau.dk>
2026-04-30 14:25:20 +0200

    finished integration test for interpreter expressions

--- test/integration/Interpreter/test_it_expressions.py
+++ test/integration/Interpreter/test_it_expressions.py
@@ -5,6 +5,38 @@ from src.ast.nodes import *
 def make_checker():
     return InterpreterVisitor()
 
+def test_it_boolean_basic_true():
+    checker = make_checker()
+    
+    # True and True or True xor True
+    node = OrExpr(AndExpr(BoolLiteral(True),BoolLiteral(True)),BoolLiteral(False))
+    
+    assert str(checker.visit(node)) == str(RuntimeValue("bool",True))
+
+def test_it_boolean_basic_false():
+    checker = make_checker()
+    
+    # True and False or True xor True
+    node = OrExpr(AndExpr(BoolLiteral(True),BoolLiteral(False)),XorExpr(BoolLiteral(False),BoolLiteral(False)))
+    
+    assert str(checker.visit(node)) == str(RuntimeValue("bool",False))
+
+def test_it_boolean_comparison_true():
+    checker = make_checker()
+    
+    # ((8>1)==(7>=7)) or (5<2 and 1<=9)
+    node = OrExpr(EqualExpr(GreaterExpr(IntLiteral(8),IntLiteral(1)),GreaterEqualExpr(IntLiteral(7),IntLiteral(7))),AndExpr(LessExpr(IntLiteral(5),IntLiteral(2)),LessEqualExpr(IntLiteral(1),IntLiteral(9))))
+    
+    assert str(checker.visit(node)) == str(RuntimeValue("bool",True))
+
+def test_it_boolean_comparison_false():
+    checker = make_checker()
+    
+    # ((1>8)==(7>=7)) or (5<2 and 1<=9)
+    node = OrExpr(EqualExpr(GreaterExpr(IntLiteral(1),IntLiteral(8)),GreaterEqualExpr(IntLiteral(7),IntLiteral(7))),AndExpr(LessExpr(IntLiteral(5),IntLiteral(2)),LessEqualExpr(IntLiteral(1),IntLiteral(9))))
+    
+    assert str(checker.visit(node)) == str(RuntimeValue("bool",False))
+    
 def test_it_pass_arithmetic_basic():
     checker = make_checker()
     
@@ -27,16 +59,17 @@ def test_it_pass_arithmetic_list_call():
     
     assert checker.lookup_var("Answer") == float(0.2)
 
-def test_it_pass_arithmetic_var_struct():
+def test_it_pass_arithmetic_var_struct_between():
     checker = make_checker()
+    random.seed(0) # need to seed between to avoid randomness in tests
     
-    # ((Y.Z+X*(4+1))^Y.Z-(8/(Y.Z+Y.Z)*(X+5)))+((6^Y.Z-4*X)/(1+1))^Y.Z
+    # ((Y.Z+X*(4+random(1,10)))^Y.Z-(8/(Y.Z+Y.Z)*(X+5)))+((6^Y.Z-4*X)/(1+1))^Y.Z
     nodes = [
         CreateVariable("Answer"),
         CreateVariable("X",IntLiteral(3)),
         CreateStruct("Y",[None,[CreateVariable("Z",IntLiteral(2))]]),
-        Assign("Answer", None, Add(Add(Pow(Add(Var("Z","Y"),Mul(Var("X"),Add(IntLiteral(4),IntLiteral(1)))),Var("Z","Y")),Neg(Mul(Div(IntLiteral(8),Add(Var("Z","Y"),Var("Z","Y"))),Add(Var("X"),IntLiteral(5))))),Pow(Div(Add(Pow(IntLiteral(6),Var("Z","Y")),Neg(Mul(IntLiteral(4),Var("X")))),Add(IntLiteral(1),IntLiteral(1))),Var("Z","Y"))))
+        Assign("Answer", None, Add(Add(Pow(Add(Var("Z","Y"),Mul(Var("X"),Add(IntLiteral(4),Between(IntLiteral(1),IntLiteral(10))))),Var("Z","Y")),Neg(Mul(Div(IntLiteral(8),Add(Var("Z","Y"),Var("Z","Y"))),Add(Var("X"),IntLiteral(5))))),Pow(Div(Add(Pow(IntLiteral(6),Var("Z","Y")),Neg(Mul(IntLiteral(4),Var("X")))),Add(IntLiteral(1),IntLiteral(1))),Var("Z","Y"))))
     ]
     checker.visit(nodes)
     
-    assert checker.lookup_var("Answer") == int(417)
+    assert checker.lookup_var("Answer") == int(1353)

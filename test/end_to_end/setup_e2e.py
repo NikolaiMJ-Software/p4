eb4c85933f5cce1abb7b0002601f8724ce87af29
Fr3Chr <166957195+Fzz1n@users.noreply.github.com>
2026-05-04 13:09:50 +0200

    remove save file after each test

--- test/end_to_end/setup_e2e.py
+++ test/end_to_end/setup_e2e.py
@@ -1,9 +1,8 @@
-import builtins
+import builtins, os, pytest
 
 from src.ast import builder
 from src.parser import parse
 from src.visitors.interpreter import InterpreterVisitor
-
 
 def run_program(code, monkeypatch, capsys, inputs=None, slot=999):
     inputs = iter(inputs or [])
@@ -16,3 +15,11 @@ def run_program(code, monkeypatch, capsys, inputs=None, slot=999):
     interp.run(ast)
 
     return capsys.readouterr().out.strip().splitlines()
+
+@pytest.fixture(autouse=True)
+def delete_save_file():
+    yield
+    try:
+        os.remove(f"src/runtime/save_states/save_slot_999.json")
+    except Exception as e:
+        print("Error, when trying to delete a file:", e)
--- test/end_to_end/test_e2e_game_state.py
+++ test/end_to_end/test_e2e_game_state.py
@@ -1,13 +1,11 @@
 import pytest
 
-from setup_e2e import run_program
+from setup_e2e import *
 from src.errors import TypeError as TypeCheckError
 from src.errors import InterpreterError
 
 
 def test_e2e_save_and_load_game_state(monkeypatch, capsys):
-    slot = 4
-
     first_code = '''create Game with:
     Game_status is 0
     Name
@@ -17,7 +15,7 @@ define Play:
     Name from Game is "Bob"
     output "saved"
 '''
-    output = run_program(first_code, monkeypatch, capsys, slot=slot)
+    output = run_program(first_code, monkeypatch, capsys)
 
     assert output == ["saved"]
 
@@ -30,14 +28,12 @@ define Play:
     output Name from Game
 '''
 
-    output = run_program(second_code, monkeypatch, capsys, slot=slot)
+    output = run_program(second_code, monkeypatch, capsys)
 
     assert output == ["1", "Bob"]
 
 
 def test_e2e_type_error_does_not_save_broken_game_state(monkeypatch, capsys):
-    slot = 3
-
     first_code = '''create Game with:
     Name
 
@@ -46,7 +42,7 @@ define Play:
     output "saved"
 '''
 
-    output = run_program(first_code, monkeypatch, capsys, slot=slot)
+    output = run_program(first_code, monkeypatch, capsys)
     assert output == ["saved"]
 
     broken_code = '''create Game with:
@@ -58,7 +54,7 @@ define Play:
 '''
 
     with pytest.raises(TypeCheckError, match="The variable: 'MissingVariable' does not exist"):
-        run_program(broken_code, monkeypatch, capsys, slot=slot)
+        run_program(broken_code, monkeypatch, capsys)
 
     third_code = '''create Game with:
     Name
@@ -67,7 +63,7 @@ define Play:
     output Name from Game
 '''
 
-    output = run_program(third_code, monkeypatch, capsys, slot=slot)
+    output = run_program(third_code, monkeypatch, capsys)
     assert output == ["Bob"]
 
 
@@ -91,8 +87,7 @@ define Play:
     else do:
         output "Invalid class"
 '''
-
-    output = run_program(code, monkeypatch, capsys, inputs=["warrior"], slot=5)
+    output = run_program(code, monkeypatch, capsys, inputs=["warrior"])
 
     assert output == [
         "Choose class",

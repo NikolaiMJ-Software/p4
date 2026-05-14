a946ea5956e8fa2ae594e03a0129d17b31c61504
Fzz1n <02frederik@gmail.com>
2026-05-14 12:37:50 +0200

    update to fit new format (remove old v_table/f_tabel from type_checker)

--- test/unit/interpreter/test_typechecking.py
+++ test/unit/interpreter/test_typechecking.py
@@ -5,42 +5,6 @@ from src.ast.nodes import *
 def make_checker():
     return InterpreterVisitor()
 
-def test_runtimetype_empty():
-    checker = make_checker()
-    
-    val = None
-    
-    assert checker.runtime_to_type(val) == None
-
-def test_runtimetype_runtimevalue():
-    checker = make_checker()
-    
-    val = RuntimeValue("int", 1)
-    
-    assert checker.runtime_to_type(val) == "int"
-
-def test_runtimetype_list():
-    checker = make_checker()
-    
-    val = [RuntimeValue("bool",1),RuntimeValue("int",2),RuntimeValue("str","3")]
-    
-    assert checker.runtime_to_type(val) == ["bool","int","str"]
-
-def test_runtimetype_dict():
-    checker = make_checker()
-    
-    val = {"X":RuntimeValue("int",1)}
-    
-    assert checker.runtime_to_type(val) == {"X":"int"}
-
-def test_sync_typechecker():
-    checker = make_checker()
-    
-    checker.v_table = {"X":RuntimeValue("bool",1),"Y":RuntimeValue("int",2),"__parent__":{"X":RuntimeValue("int",0),"Z":RuntimeValue("str","3")}}
-    checker.sync_type_checker()
-    
-    assert checker.type_checker.v_table == {'X': 'bool', 'Y': 'int', '__parent__': {'X': 'int', 'Z': 'str'}}
-
 def test_unwrap():
     checker = make_checker()
     
@@ -48,10 +12,9 @@ def test_unwrap():
     
     assert checker.unwrap(val) == 1
 
-def test_checkexpressiontype():
+def test_unwrap_list():
     checker = make_checker()
     
-    checker.v_table = {"X":RuntimeValue("bool",0)}
-    node = Var("X")
+    val = [RuntimeValue("float",4.4), [RuntimeValue("int",1), RuntimeValue("bool",True), [RuntimeValue("str","Bob"), RuntimeValue("int",32)]], RuntimeValue("str","Hell"), [RuntimeValue("float",7.6), RuntimeValue("str","69")]]
     
-    assert checker.check_expression_type(node) == "bool"
+    assert checker.unwrap_list(val) == [4.4, [1, True, ['Bob', 32]], 'Hell', [7.6, '69']]

1111c9b001cc5ad71817b8d8c8de539b3f8f8f84
Sebastian Lukasz Brechun <94458257+Sebastianplaygames@users.noreply.github.com>
2026-05-25 04:19:26 +0200

    fixed directory after prior path mistake in the push

--- test/end_to_end/setup_e2e.py
+++ test/end_to_end/setup_e2e.py
@@ -26,6 +26,6 @@ def run_program(code, monkeypatch, capsys, inputs=None, slot=999):
 def delete_save_file():
     yield
     try:
-        os.remove(f"p4/src/runtime/save_states/save_slot_999.json")
+        os.remove(f"src/runtime/save_states/save_slot_999.json")
     except Exception as e:
         print("Error, when trying to delete a file:", e)
